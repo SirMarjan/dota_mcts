5cff7da211a92fd5bacfe1a19831abe7d193d7a9
Roudie <sebak19962@gmail.com>
2020-05-20 10:03:18 +0200

    save

--- player.py
+++ player.py
@@ -58,7 +58,7 @@ class MCTSPlayer(Player):
 
     def __init__(self, name, draft, maxiters, c):
         self.draft = draft
-        self.name =name
+        self.name = name
         self.maxiters = maxiters
         self.c = c
 
@@ -113,97 +113,3 @@ class MCTSPlayer(Player):
             # logger.info('')
 
         return root.select_final()
-
-
-class AssocRulePlayer(Player):
-
-    def __init__(self, draft):
-        self.draft = draft
-        self.name = 'assocrule'
-        self.load_rules(match_num=3056596,
-                        oppo_team_spmf_path='apriori/dota_oppo_team_output.txt',
-                        win_team_spmf_path='apriori/dota_win_team_output.txt',
-                        lose_team_spmf_path='apriori/dota_lose_team_output.txt')
-
-    def load_rules(self, match_num, oppo_team_spmf_path, win_team_spmf_path, lose_team_spmf_path):
-        self.oppo_1_rules = dict()
-        self.oppo_2_rules = dict()
-        with open(oppo_team_spmf_path, 'r') as f:
-            for line in f:
-                items, support = line.split(' #SUP: ')
-                items, support = list(map(int, items.strip().split(' '))), int(support.strip())
-                # S(-e), because -e is losing champion encoded in 1xxx
-                if len(items) == 1 and items[0] > 1000:
-                    self.oppo_1_rules[frozenset(items)] = support / match_num
-                elif len(items) == 2 and (items[0] < 1000 and items[1] > 1000):
-                    self.oppo_2_rules[frozenset(items)] = support / match_num
-                else:
-                    continue
-
-        self.win_rules = dict()
-        with open(win_team_spmf_path, 'r') as f:
-            for line in f:
-                items, support = line.split(' #SUP: ')
-                items, support = list(map(int, items.strip().split(' '))), int(support.strip())
-                if len(items) == 1:
-                    continue
-                self.win_rules[frozenset(items)] = support / match_num
-
-        self.lose_rules = dict()
-        with open(lose_team_spmf_path, 'r') as f:
-            for line in f:
-                items, support = line.split(' #SUP: ')
-                items, support = list(map(int, items.strip().split(' '))), int(support.strip())
-                if len(items) == 1:
-                    continue
-                self.lose_rules[frozenset(items)] = support / match_num
-
-    def get_move(self, move_type):
-        if self.draft.if_first_move():
-            return self.get_first_move()
-
-        player = self.draft.next_player
-        # if ban, we are selecting the best hero for opponent
-        if move_type == 'ban':
-            player = player ^ 1
-        allies = frozenset(self.draft.get_state(player))
-        oppo_player = player ^ 1
-        # enemy id needs to add 1000
-        enemies = frozenset([i+1000 for i in self.draft.get_state(oppo_player)])
-
-        R = list()
-
-        ally_candidates = list()
-        for key in self.win_rules:
-            intercept = allies & key
-            assoc = key - intercept
-            if len(intercept) > 0 and len(assoc) == 1:
-                assoc = next(iter(assoc))  # extract the move from the set
-                if assoc in self.draft.get_moves():
-                    win_sup = self.win_rules[key]
-                    lose_sup = self.lose_rules.get(key, 0.0)   # lose support may not exist
-                    win_rate = win_sup / (win_sup + lose_sup)
-                    ally_candidates.append((allies, key, assoc, win_rate))
-        # select top 5 win rate association rules
-        ally_candidates = sorted(ally_candidates, key=lambda x: x[-1])[-5:]
-        R.extend([a[-2] for a in ally_candidates])
-
-        enemy_candidates = list()
-        for key in self.oppo_2_rules:
-            intercept = enemies & key
-            assoc = key - intercept
-            if len(intercept) == 1 and len(assoc) == 1:
-                assoc = next(iter(assoc))       # extract the move from the set
-                if assoc in self.draft.get_moves():
-                    confidence = self.oppo_2_rules[key] / self.oppo_1_rules[intercept]
-                    enemy_candidates.append((enemies, key, assoc, confidence))
-        # select top 5 confidence association rules
-        enemy_candidates = sorted(enemy_candidates, key=lambda x: x[-1])[-5:]
-        R.extend([e[-2] for e in enemy_candidates])
-
-        if len(R) == 0:
-            moves = self.draft.get_moves()
-            return random.sample(moves, 1)[0]
-        else:
-            move = random.choice(R)
-            return move
